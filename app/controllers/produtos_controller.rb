a86ddd555abbd2336a45699feeeeebaee38b17ce
Estrela <estrela.steinkirch@gmail.com>
2019-07-20 11:33:55 +0200

    Produtos controller

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,2 +1,9 @@
 class ProdutosController < ApplicationController
+
+  def index
+    # mesmo nome da pag index
+    @produtos = Produto.order(nome: :desc).limit 2
+    # o @ significa que é uma variável de instância
+    @produto_com_desconto = Produto.order(:preco).limit 1
+  end
 end
