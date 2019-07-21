04d74f31b6c332f283f05d2218676bd9d30b8998
Estrela <estrela.steinkirch@gmail.com>
2019-07-21 23:12:24 +0200

    Edit e Update para os produtos

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -24,6 +24,27 @@ class ProdutosController < ApplicationController
     end
   end
 
+  def edit
+    id = params[:id]
+    @produto = Produto.find(id)
+    @departamentos = Departamento.all
+    render :new
+  end
+
+  def update
+    id = params[:id]
+    @produto = Produto.find(id)
+    valores = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade, :departamento_id)
+    if @produto.update valores
+      flash[:notice] = "Produto atualizado com sucesso!"
+      redirect_to root_path
+    else
+      @departamentos = Departamento.all
+      render :new
+    end
+
+  end
+
   def destroy
     id = params[:id]
     Produto.destroy id
