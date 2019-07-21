86d50ebaaefede9e79a2f4ab19e7c08029cc9eb5
Estrela <estrela.steinkirch@gmail.com>
2019-07-21 22:04:59 +0200

    Cria tabela Departamentos

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -10,10 +10,11 @@ class ProdutosController < ApplicationController
 
   def new
     @produto = Produto.new
+    @departamentos = Departamento.all
   end
 
   def create
-    valores = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade)
+    valores = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade, :departamento_id)
     @produto = Produto.new valores
     if @produto.save
       flash[:notice] = "Produto salvo com sucesso!"
