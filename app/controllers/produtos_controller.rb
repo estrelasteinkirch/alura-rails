27b2acc7314e2e63e35d9374176a7eee18528a24
Estrela <estrela.steinkirch@gmail.com>
2019-07-21 12:11:06 +0200

    Criar novo produto

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -2,8 +2,16 @@ class ProdutosController < ApplicationController
 
   def index
     # mesmo nome da pag index
-    @produtos = Produto.order(nome: :desc).limit 2
+    @produtos = Produto.order(nome: :desc).limit(5)
     # o @ significa que é uma variável de instância
-    @produto_com_desconto = Produto.order(:preco).limit 1
+    # que é compartilhado com todo o objeto
+    @produto_com_desconto = Produto.order(:preco).limit(1)
   end
+
+  def create
+    produto = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade)
+    Produto.create produto
+    redirect_to root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
- root to: "produtos#index"
+  post "produtos", to: "produtos#create"
+  get "produtos/new", to: "produtos#new"
+  root to: "produtos#index"
+  # resource :produtos
 end
